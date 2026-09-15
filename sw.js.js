5a5fc8f325698f99672954f3e69101700be7879f
hcessa <engr.abulfathi@gmail.com>
2026-09-15 09:47:23 +0100

    Add files via upload

---
+++ sw.js.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'ago-calc-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install Event - Caches essential files locally
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Fetch Event - Serves files strictly from local cache (100% offline)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
